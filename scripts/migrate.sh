3339f3910cd9267220354aaf8e523f762e28c220
sodefrin <kojitazoe@gmail.com>
2020-04-04 02:17:58 +0900

    update circleci

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -8,7 +8,7 @@ gcloud config set auth/disable_credentials true
 gcloud config set project $SPANNER_PROJECT_ID
 gcloud config set api_endpoint_overrides/spanner http://localhost:9020/
 
-#gcloud spanner instances create $SPANNER_INSTANCE_ID --project=$SPANNER_PROJECT_ID --config=regional-us-central1 --description="" --nodes=1
-#gcloud spanner databases create $SPANNER_DATABASE_ID --project=$SPANNER_PROJECT_ID --instance=$SPANNER_INSTANCE_ID
+gcloud spanner instances create $SPANNER_INSTANCE_ID --config=regional-us-central1 --description="" --nodes=1
+gcloud spanner databases create $SPANNER_DATABASE_ID --instance=$SPANNER_INSTANCE_ID
 
-#gcloud spanner databases ddl update $SPANNER_DATABASE_ID --project=$SPANNER_PROJECT_ID --instance=$SPANNER_INSTANCE_ID --ddl="`cat $DDL`"
+gcloud spanner databases ddl update $SPANNER_DATABASE_ID --project=$SPANNER_PROJECT_ID --instance=$SPANNER_INSTANCE_ID --ddl="`cat $DDL`"
