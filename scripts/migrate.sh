e1ebd8a75d803ac4fe5f0534b0a3432b37b8869f
sodefrin <kojitazoe@gmail.com>
2020-04-04 01:10:24 +0900

    update circle ci

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,5 +1,8 @@
 !/bin/sh
 
+gcloud components update beta
+gcloud beta emulators spanner start
+
 gcloud config configurations create emulator
 gcloud config set auth/disable_credentials true
 gcloud config set project $SPANNER_PROJECT_ID
