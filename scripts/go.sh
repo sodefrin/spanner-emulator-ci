90f9742235855eeeac22c82d080104cc054bdea1
sodefrin <kojitazoe@gmail.com>
2020-04-04 04:24:56 +0900

    update

--- scripts/go.sh
+++ scripts/go.sh
@@ -2,4 +2,4 @@
 
 curl -O https://dl.google.com/go/go1.14.1.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.14.1.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> .bashrc
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
